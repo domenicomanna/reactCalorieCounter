9fff326c7247e01f259c55f39442a49c83e7045a
Domenico Manna <mannadomenico2849@gmail.com>
2019-05-08 18:00:37 -0400

    Add food input

---
+++ src/CalorieCounter/FoodInput.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import '../index.css';
+import './CalorieCounter.css';
+
+export default function FoodInput() {
+    return (
+        <ul className="list calorie-counter__group calorie-counter__inputs">
+            <li><input type="text" className="calorie-counter__food-input" placeholder="Description" /></li>
+            <li><input type="text" className="calorie-counter__food-input" placeholder="Calories" /></li>
+            <li><input type="text" className="calorie-counter__food-input" placeholder="Fat" /></li>
+            <li className="calorie-counter__add button button--add">
+                <i className="fas fa-plus"></i>
+            </li>
+            <li className="calorie-counter__label"><i className="fas fa-times calorie-counter__dummy-icon"></i></li>
+        </ul>
+    )
+}
