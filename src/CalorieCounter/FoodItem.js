b88e34d24687999d352ff3d6c8de32ddde0cffc5
Domenico Manna <mannadomenico2849@gmail.com>
2019-05-08 17:54:13 -0400

    Add food list

---
+++ src/CalorieCounter/FoodItem.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import '../index.css'
+import './CalorieCounter.css'
+
+export default function FoodItem() {
+    return (
+        <ul className="list calorie-counter__group">
+            <li><span className="calorie-counter__food-detail">hi</span></li>
+            <li><span className="calorie-counter__food-detail">hi</span></li>
+            <li><span className="calorie-counter__food-detail">hi</span></li>
+            <li className="calorie-counter__label">
+                <i className="fas fa-times calorie-counter__delete button"></i>
+            </li>
+        </ul>
+    );
+}
---
+++ src/CalorieCounter/FoodList.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../index.css'
+import './CalorieCounter.css'
+import FoodItem from './FoodItem';
+
+export default function FoodList() {
+    return (
+        <ul className="list calorie-counter__list-items">
+            <FoodItem />
+            <FoodItem />
+        </ul>
+    );
+}
