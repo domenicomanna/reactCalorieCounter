9caf1c03c3d874e61b8e94123cfcff6a3d9d6ae6
Domenico Manna <mannadomenico2849@gmail.com>
2019-05-08 18:20:05 -0400

    Adjust styling

--- src/CalorieCounter/FoodInput.js
+++ src/CalorieCounter/FoodInput.js
@@ -6,8 +6,8 @@ export default function FoodInput() {
     return (
         <ul className="list calorie-counter__group calorie-counter__inputs">
             <li><input type="text" className="calorie-counter__food-input" placeholder="Description" /></li>
-            <li><input type="text" className="calorie-counter__food-input" placeholder="Calories" /></li>
-            <li><input type="text" className="calorie-counter__food-input" placeholder="Fat" /></li>
+            <li><input type="number" className="calorie-counter__food-input" placeholder="Calories" /></li>
+            <li><input type="number" className="calorie-counter__food-input" placeholder="Fat" /></li>
             <li className="calorie-counter__add button button--add">
                 <i className="fas fa-plus"></i>
             </li>
--- src/CalorieCounter/FoodItem.js
+++ src/CalorieCounter/FoodItem.js
@@ -4,13 +4,15 @@ import './CalorieCounter.css'
 
 export default function FoodItem() {
     return (
-        <ul className="list calorie-counter__group">
-            <li><span className="calorie-counter__food-detail">hi</span></li>
-            <li><span className="calorie-counter__food-detail">hi</span></li>
-            <li><span className="calorie-counter__food-detail">hi</span></li>
-            <li className="calorie-counter__label">
-                <i className="fas fa-times calorie-counter__delete button"></i>
-            </li>
-        </ul>
+        <li className="calorie-counter__food-item">
+            <ul className="list calorie-counter__group">
+                <li><span className="calorie-counter__food-detail">hi</span></li>
+                <li><span className="calorie-counter__food-detail">hi</span></li>
+                <li><span className="calorie-counter__food-detail">hi</span></li>
+                <li className="calorie-counter__label">
+                    <i className="fas fa-times calorie-counter__delete button"></i>
+                </li>
+            </ul>
+        </li>
     );
 }
