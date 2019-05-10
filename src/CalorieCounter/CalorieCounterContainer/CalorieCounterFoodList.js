2a395af2b6fd2dbc2fe74fd5a835b070cb8e7b78
Domenico Manna <mannadomenico2849@gmail.com>
2019-05-10 18:24:26 -0400

    Adjust style when there are no food items present

--- src/CalorieCounter/CalorieCounterContainer/CalorieCounterFoodList.js
+++ src/CalorieCounter/CalorieCounterContainer/CalorieCounterFoodList.js
@@ -4,15 +4,21 @@ import '../CalorieCounter.css';
 import CalorieCounterFoodItem from './CalorieCounterFoodItem';
 
 export default function CalorieCounterFoodList(props) {
-    let foods = props.foodItems.map( (food, index) => {
+    if (props.foodItems.length === 0) var foodListStyle = { display: "none" };
+
+    let foods = props.foodItems.map((food, index) => {
         return <CalorieCounterFoodItem key={food.id}
             description={food.description}
             calories={food.calories}
             fat={food.fat}
-            handleFoodDeletion = {props.handleFoodDeletion} 
-            index = {index}/>
+            handleFoodDeletion={props.handleFoodDeletion}
+            index={index} />
     })
 
-    return <ul className="list calorie-counter__list-items"> {foods} </ul>;
+    return (
+        <ul className="list calorie-counter__list-items"
+            style={foodListStyle}> {foods}
+        </ul>
+    );
 
 }
