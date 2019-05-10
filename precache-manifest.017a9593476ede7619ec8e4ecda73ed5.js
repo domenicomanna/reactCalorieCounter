90d13915eff2fc06f048092bfe48375c5388586c
Domenico Manna <mannadomenico2849@gmail.com>
2019-05-10 18:38:59 -0400

    Updates

---
+++ precache-manifest.017a9593476ede7619ec8e4ecda73ed5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1a7f5329eb4023b8f0d538c31180158",
+    "url": "/reactCalorieCounter/index.html"
+  },
+  {
+    "revision": "763ace8d43b4be21f25a",
+    "url": "/reactCalorieCounter/static/css/main.fffdbaa4.chunk.css"
+  },
+  {
+    "revision": "4f292bacc541508244c0",
+    "url": "/reactCalorieCounter/static/js/2.d9a5798b.chunk.js"
+  },
+  {
+    "revision": "763ace8d43b4be21f25a",
+    "url": "/reactCalorieCounter/static/js/main.bfe02253.chunk.js"
+  },
+  {
+    "revision": "db307c4f4ae25c86f3ce",
+    "url": "/reactCalorieCounter/static/js/runtime~main.85ebe8c8.js"
+  }
+]);
